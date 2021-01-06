58d46f7d72d8736b6013b8c2e649917d33415653
Klius <Kliusescarlopini@gmail.com>
2021-01-06 14:35:28 +0100

    first commit saving, after loading doesn't work

---
+++ gen1.js
@@ -0,0 +1,709 @@
+[
+    {
+        "index": "001",
+        "name": "Bulbasaur",
+        "gotcha": false
+    },
+    {
+        "index": "002",
+        "name": "Ivysaur",
+        "image_url": "http://serebii.net/Pkmn_Red/002.gif"
+
+    },
+    {
+        "index": "003",
+        "name": "Venusaur",
+        "image_url": "http://serebii.net/Pkmn_Red/003.gif"
+    },
+    {
+        "index": "004",
+        "name": "Charmander",
+        "image_url": "http://serebii.net/Pkmn_Red/004.gif"
+    },
+    {
+        "index": "005",
+        "name": "Charmeleon",
+        "image_url": "http://serebii.net/Pkmn_Red/005.gif"
+    },
+    {
+        "index": "006",
+        "name": "Charizard",
+        "image_url": "http://serebii.net/Pkmn_Red/006.gif"
+    },
+    {
+        "index": "007",
+        "name": "Squirtle",
+        "image_url": "http://serebii.net/Pkmn_Red/007.gif"
+    },
+    {
+        "index": "008",
+        "name": "Wartortle",
+        "image_url": "http://serebii.net/Pkmn_Red/008.gif"
+    },
+    {
+        "index": "009",
+        "name": "Blastoise",
+        "image_url": "http://serebii.net/Pkmn_Red/009.gif"
+    },
+    {
+        "index": "010",
+        "name": "Caterpie",
+        "image_url": "http://serebii.net/Pkmn_Red/010.gif"
+    },
+    {
+        "index": "011",
+        "name": "Metapod",
+        "image_url": "http://serebii.net/Pkmn_Red/011.gif"
+    },
+    {
+        "index": "012",
+        "name": "Butterfree",
+        "image_url": "http://serebii.net/Pkmn_Red/012.gif"
+    },
+    {
+        "index": "013",
+        "name": "Weedle",
+        "image_url": "http://serebii.net/Pkmn_Red/013.gif"
+    },
+    {
+        "index": "014",
+        "name": "Kakuna",
+        "image_url": "http://serebii.net/Pkmn_Red/014.gif"
+    },
+    {
+        "index": "015",
+        "name": "Beedrill",
+        "image_url": "http://serebii.net/Pkmn_Red/015.gif"
+    },
+    {
+        "index": "016",
+        "name": "Pidgey",
+        "image_url": "http://serebii.net/Pkmn_Red/016.gif"
+    },
+    {
+        "index": "017",
+        "name": "Pidgeotto",
+        "image_url": "http://serebii.net/Pkmn_Red/017.gif"
+    },
+    {
+        "index": "018",
+        "name": "Pidgeot",
+        "image_url": "http://serebii.net/Pkmn_Red/018.gif"
+    },
+    {
+        "index": "019",
+        "name": "Rattata",
+        "image_url": "http://serebii.net/Pkmn_Red/019.gif"
+    },
+    {
+        "index": "020",
+        "name": "Ratticate",
+        "image_url": "http://serebii.net/Pkmn_Red/020.gif"
+    },
+    {
+        "index": "021",
+        "name": "Spearow",
+        "image_url": "http://serebii.net/Pkmn_Red/021.gif"
+    },
+    {
+        "index": "022",
+        "name": "Fearow",
+        "image_url": "http://serebii.net/Pkmn_Red/022.gif"
+    },
+    {
+        "index": "023",
+        "name": "Ekans",
+        "image_url": "http://serebii.net/Pkmn_Red/023.gif"
+    },
+    {
+        "index": "024",
+        "name": "Arbok",
+        "image_url": "http://serebii.net/Pkmn_Red/024.gif"
+    },
+    {
+        "index": "025",
+        "name": "Pikachu",
+        "image_url": "http://serebii.net/Pkmn_Red/025.gif"
+    },
+    {
+        "index": "026",
+        "name": "Raichu",
+        "image_url": "http://serebii.net/Pkmn_Red/026.gif"
+    },
+    {
+        "index": "027",
+        "name": "Sandshrew",
+        "image_url": "http://serebii.net/Pkmn_Red/027.gif"
+    },
+    {
+        "index": "028",
+        "name": "Sandslash",
+        "image_url": "http://serebii.net/Pkmn_Red/028.gif"
+    },
+    {
+        "index": "029",
+        "name": "Nidoran(F)",
+        "image_url": "http://serebii.net/Pkmn_Red/029.gif"
+    },
+    {
+        "index": "030",
+        "name": "Nidorina",
+        "image_url": "http://serebii.net/Pkmn_Red/030.gif"
+    },
+    {
+        "index": "031",
+        "name": "Nidoqueen",
+        "image_url": "http://serebii.net/Pkmn_Red/031.gif"
+    },
+    {
+        "index": "032",
+        "name": "Nidoran(M)",
+        "image_url": "http://serebii.net/Pkmn_Red/032.gif"
+    },
+    {
+        "index": "033",
+        "name": "Nidorino",
+        "image_url": "http://serebii.net/Pkmn_Red/033.gif"
+    },
+    {
+        "index": "034",
+        "name": "Nidoking",
+        "image_url": "http://serebii.net/Pkmn_Red/034.gif"
+    },
+    {
+        "index": "035",
+        "name": "Clefairy",
+        "image_url": "http://serebii.net/Pkmn_Red/035.gif"
+    },
+    {
+        "index": "036",
+        "name": "Clefable",
+        "image_url": "http://serebii.net/Pkmn_Red/036.gif"
+    },
+    {
+        "index": "037",
+        "name": "Vulpix",
+        "image_url": "http://serebii.net/Pkmn_Red/037.gif"
+    },
+    {
+        "index": "038",
+        "name": "Ninetales",
+        "image_url": "http://serebii.net/Pkmn_Red/038.gif"
+    },
+    {
+        "index": "039",
+        "name": "Jigglypuff",
+        "image_url": "http://serebii.net/Pkmn_Red/039.gif"
+    },
+    {
+        "index": "040",
+        "name": "Wigglytuff",
+        "image_url": "http://serebii.net/Pkmn_Red/040.gif"
+    },
+    {
+        "index": "041",
+        "name": "Zubat",
+        "image_url": "http://serebii.net/Pkmn_Red/041.gif"
+    },
+    {
+        "index": "042",
+        "name": "Golbat",
+        "image_url": "http://serebii.net/Pkmn_Red/042.gif"
+    },
+    {
+        "index": "043",
+        "name": "Oddish",
+        "image_url": "http://serebii.net/Pkmn_Red/043.gif"
+    },
+    {
+        "index": "044",
+        "name": "Gloom",
+        "image_url": "http://serebii.net/Pkmn_Red/044.gif"
+    },
+    {
+        "index": "045",
+        "name": "Vileplume",
+        "image_url": "http://serebii.net/Pkmn_Red/045.gif"
+    },
+    {
+        "index": "046",
+        "name": "Paras",
+        "image_url": "http://serebii.net/Pkmn_Red/046.gif"
+    },
+    {
+        "index": "047",
+        "name": "Parasect",
+        "image_url": "http://serebii.net/Pkmn_Red/047.gif"
+    },
+    {
+        "index": "048",
+        "name": "Venonat",
+        "image_url": "http://serebii.net/Pkmn_Red/048.gif"
+    },
+    {
+        "index": "049",
+        "name": "Venomoth",
+        "image_url": "http://serebii.net/Pkmn_Red/049.gif"
+    },
+    {
+        "index": "050",
+        "name": "Digglet",
+        "image_url": "http://serebii.net/Pkmn_Red/050.gif"
+    },
+    {
+        "index": "051",
+        "name": "Dugtrio",
+        "image_url": "http://serebii.net/Pkmn_Red/051.gif"
+    },
+    {
+        "index": "052",
+        "name": "Meowth",
+        "image_url": "http://serebii.net/Pkmn_Red/052.gif"
+    },
+    {
+        "index": "053",
+        "name": "Persian",
+        "image_url": "http://serebii.net/Pkmn_Red/053.gif"
+    },
+    {
+        "index": "054",
+        "name": "Psyduck",
+        "image_url": "http://serebii.net/Pkmn_Red/054.gif"
+    }, {
+        "index": "055",
+        "name": "Golduck",
+        "image_url": "http://serebii.net/Pkmn_Red/055.gif"
+    },
+    {
+        "index": "056",
+        "name": "Mankey",
+        "image_url": "http://serebii.net/Pkmn_Red/056.gif"
+    },
+    {
+        "index": "057",
+        "name": "Primeape",
+        "image_url": "http://serebii.net/Pkmn_Red/057.gif"
+    },
+    {
+        "index": "058",
+        "name": "Growlite",
+        "image_url": "http://serebii.net/Pkmn_Red/058.gif"
+    },
+    {
+        "index": "059",
+        "name": "Arcanine",
+        "image_url": "http://serebii.net/Pkmn_Red/059.gif"
+    },
+    {
+        "index": "060",
+        "name": "Poliwag",
+        "image_url": "http://serebii.net/Pkmn_Red/060.gif"
+    },
+    {
+        "index": "061",
+        "name": "Poliwhirl",
+        "image_url": "http://serebii.net/Pkmn_Red/061.gif"
+    },
+    {
+        "index": "062",
+        "name": "Poliwrath",
+        "image_url": "http://serebii.net/Pkmn_Red/062.gif"
+    },
+    {
+        "index": "063",
+        "name": "Abra",
+        "image_url": "http://serebii.net/Pkmn_Red/063.gif"
+    },
+    {
+        "index": "064",
+        "name": "Kadabra",
+        "image_url": "http://serebii.net/Pkmn_Red/064.gif"
+    },
+    {
+        "index": "065",
+        "name": "Alakazam",
+        "image_url": "http://serebii.net/Pkmn_Red/065.gif"
+    },
+    {
+        "index": "066",
+        "name": "Machop",
+        "image_url": "http://serebii.net/Pkmn_Red/066.gif"
+    },
+    {
+        "index": "067",
+        "name": "Machoke",
+        "image_url": "http://serebii.net/Pkmn_Red/067.gif"
+    },
+    {
+        "index": "068",
+        "name": "Machamp",
+        "image_url": "http://serebii.net/Pkmn_Red/068.gif"
+    },
+    {
+        "index": "069",
+        "name": "Bellsprout",
+        "image_url": "http://serebii.net/Pkmn_Red/069.gif"
+    },
+    {
+        "index": "070",
+        "name": "Weepinbell",
+        "image_url": "http://serebii.net/Pkmn_Red/070.gif"
+    },
+    {
+        "index": "071",
+        "name": "Victreebel",
+        "image_url": "http://serebii.net/Pkmn_Red/071.gif"
+    },
+    {
+        "index": "072",
+        "name": "Tentacool",
+        "image_url": "http://serebii.net/Pkmn_Red/072.gif"
+    },
+    {
+        "index": "073",
+        "name": "Tentacruel",
+        "image_url": "http://serebii.net/Pkmn_Red/073.gif"
+    },
+    {
+        "index": "074",
+        "name": "Geodude",
+        "image_url": "http://serebii.net/Pkmn_Red/074.gif"
+    },
+    {
+        "index": "075",
+        "name": "Graveler",
+        "image_url": "http://serebii.net/Pkmn_Red/075.gif"
+    },
+    {
+        "index": "076",
+        "name": "Golem",
+        "image_url": "http://serebii.net/Pkmn_Red/076.gif"
+    },
+    {
+        "index": "077",
+        "name": "Ponyta",
+        "image_url": "http://serebii.net/Pkmn_Red/077.gif"
+    },
+    {
+        "index": "078",
+        "name": "Rapidash",
+        "image_url": "http://serebii.net/Pkmn_Red/078.gif"
+    },
+    {
+        "index": "079",
+        "name": "Slowpoke",
+        "image_url": "http://serebii.net/Pkmn_Red/079.gif"
+    },
+    {
+        "index": "080",
+        "name": "Slowbro",
+        "image_url": "http://serebii.net/Pkmn_Red/080.gif"
+    },
+    {
+        "index": "081",
+        "name": "Magnemite",
+        "image_url": "http://serebii.net/Pkmn_Red/081.gif"
+    },
+    {
+        "index": "082",
+        "name": "Magneton",
+        "image_url": "http://serebii.net/Pkmn_Red/082.gif"
+    },
+    {
+        "index": "083",
+        "name": "Farfetch'd",
+        "image_url": "http://serebii.net/Pkmn_Red/083.gif"
+    },
+    {
+        "index": "084",
+        "name": "Doduo",
+        "image_url": "http://serebii.net/Pkmn_Red/084.gif"
+    },
+    {
+        "index": "085",
+        "name": "Dodrio",
+        "image_url": "http://serebii.net/Pkmn_Red/085.gif"
+    },
+    {
+        "index": "086",
+        "name": "Seel",
+        "image_url": "http://serebii.net/Pkmn_Red/086.gif"
+    },
+    {
+        "index": "087",
+        "name": "Dewgong",
+        "image_url": "http://serebii.net/Pkmn_Red/087.gif"
+    },
+    {
+        "index": "088",
+        "name": "Grimer",
+        "image_url": "http://serebii.net/Pkmn_Red/088.gif"
+    },
+    {
+        "index": "089",
+        "name": "Muk",
+        "image_url": "http://serebii.net/Pkmn_Red/089.gif"
+    },
+    {
+        "index": "090",
+        "name": "Shellder",
+        "image_url": "http://serebii.net/Pkmn_Red/090.gif"
+    },
+    {
+        "index": "091",
+        "name": "Cloyster",
+        "image_url": "http://serebii.net/Pkmn_Red/091.gif"
+    },
+    {
+        "index": "092",
+        "name": "Ghastly",
+        "image_url": "http://serebii.net/Pkmn_Red/092.gif"
+    },
+    {
+        "index": "093",
+        "name": "Haunter",
+        "image_url": "http://serebii.net/Pkmn_Red/093.gif"
+    },
+    {
+        "index": "094",
+        "name": "Gengar",
+        "image_url": "http://serebii.net/Pkmn_Red/094.gif"
+    },
+    {
+        "index": "095",
+        "name": "Onix",
+        "image_url": "http://serebii.net/Pkmn_Red/095.gif"
+    },
+    {
+        "index": "096",
+        "name": "Drowzee",
+        "image_url": "http://serebii.net/Pkmn_Red/096.gif"
+    },
+    {
+        "index": "097",
+        "name": "Hypno",
+        "image_url": "http://serebii.net/Pkmn_Red/097.gif"
+    },
+    {
+        "index": "098",
+        "name": "Krabby",
+        "image_url": "http://serebii.net/Pkmn_Red/098.gif"
+    },
+    {
+        "index": "099",
+        "name": "Kingler",
+        "image_url": "http://serebii.net/Pkmn_Red/099.gif"
+    },
+    {
+        "index": "100",
+        "name": "Voltorb",
+        "image_url": "http://serebii.net/Pkmn_Red/100.gif"
+    },
+    {
+        "index": "101",
+        "name": "Electrode",
+        "image_url": "http://serebii.net/Pkmn_Red/101.gif"
+    },
+    {
+        "index": "102",
+        "name": "Exeggcute",
+        "image_url": "http://serebii.net/Pkmn_Red/102.gif"
+    },
+    {
+        "index": "103",
+        "name": "Exeggutor"
+    },
+    {
+        "index": "104",
+        "name": "cubone"
+    },
+    {
+        "index": "105",
+        "name": "marowak"
+    },
+    {
+        "index": "106",
+        "name": "hitmonlee"
+    },
+    {
+        "index": "107",
+        "name": "hitmonchan"
+    },
+    {
+        "index": "108",
+        "name": "lickitung"
+    },
+    {
+        "index": "109",
+        "name": "koffing"
+    },
+    {
+        "index": "110",
+        "name": "weezing"
+    },
+    {
+        "index": "111",
+        "name": "rhyhorn"
+    },
+    {
+        "index": "112",
+        "name": "rhydon"
+    },
+    {
+        "index": "113",
+        "name": "chansey"
+    },
+    {
+        "index": "114",
+        "name": "tangela"
+    },
+    {
+        "index": "115",
+        "name": "kangaskhan"
+    },
+    {
+        "index": "116",
+        "name": "horsea"
+    },
+    {
+        "index": "117",
+        "name": "seadra"
+    },
+    {
+        "index": "118",
+        "name": "goldeen"
+    },
+    {
+        "index": "119",
+        "name": "seaking"
+    },
+    {
+        "index": "120",
+        "name": "staryu"
+    },
+    {
+        "index": "121",
+        "name": "starmie"
+    },
+    {
+        "index": "122",
+        "name": "mr-mime"
+    },
+    {
+        "index": "123",
+        "name": "scyther"
+    },
+    {
+        "index": "124",
+        "name": "jynx"
+    },
+    {
+        "index": "125",
+        "name": "electabuzz"
+    },
+    {
+        "index": "126",
+        "name": "magmar"
+    },
+    {
+        "index": "127",
+        "name": "pinsir"
+    },
+    {
+        "index": "128",
+        "name": "tauros"
+    },
+    {
+        "index": "129",
+        "name": "magikarp"
+    },
+    {
+        "index": "130",
+        "name": "gyarados"
+    },
+    {
+        "index": "131",
+        "name": "lapras"
+    },
+    {
+        "index": "132",
+        "name": "ditto"
+    },
+    {
+        "index": "133",
+        "name": "eevee"
+    },
+    {
+        "index": "134",
+        "name": "vaporeon"
+    },
+    {
+        "index": "135",
+        "name": "jolteon"
+    },
+    {
+        "index": "136",
+        "name": "flareon"
+    },
+    {
+        "index": "137",
+        "name": "porygon"
+    },
+    {
+        "index": "138",
+        "name": "omanyte"
+    },
+    {
+        "index": "139",
+        "name": "omastar"
+    },
+    {
+        "index": "140",
+        "name": "kabuto"
+    },
+    {
+        "index": "141",
+        "name": "kabutops"
+    },
+    {
+        "index": "142",
+        "name": "aerodactyl"
+    },
+    {
+        "index": "143",
+        "name": "snorlax"
+    },
+    {
+        "index": "144",
+        "name": "articuno"
+    },
+    {
+        "index": "145",
+        "name": "zapdos"
+    },
+    {
+        "index": "146",
+        "name": "moltres"
+    },
+    {
+        "index": "147",
+        "name": "dratini"
+    },
+    {
+        "index": "148",
+        "name": "dragonair"
+    },
+    {
+        "index": "149",
+        "name": "dragonite"
+    },
+    {
+        "index": "150",
+        "name": "mewtwo"
+    },
+    {
+        "index": "151",
+        "name": "mew"
+    }
+
+]
